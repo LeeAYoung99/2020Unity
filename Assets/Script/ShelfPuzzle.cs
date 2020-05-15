0720d6d11479a40423d84f1213070d1fc0b77495
Yangyeeun <kap12@naver.com>
2020-05-16 06:05:20 +0900

    Shelf puzzle
    1. 액자를 3번 클릭하면 액자 커버가 사라지고 액자 퍼즐에 대한 힌트가 보임_ShlefClickCount
    2.  그 힌트를 보면 옮겨야하는 약병색이 나와있는데 그 사진대로 약병을 주어진 위치에 약병 두개를 옮겨놓으면 성공 _Shelf Puzzle
    고쳐야할점_ 카메라가 움직이지않아 실행해서 확인하지못함

---
+++ Assets/Script/ShelfPuzzle.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelfPuzzle : MonoBehaviour
+{
+    public GameObject med1,med2;
+    public GameObject  med1Des, med2Des;
+    Vector3  med1InitPos, med2InitPos;
+    //Vector3 frame1DesPos, frame2DesPos, med1DesPos, med2DesPos;
+    bool med1Correct, med2Correct=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        med1InitPos = med1.transform.position;
+        med2InitPos = med2.transform.position;
+        
+        //med1DesPos = med1Des.transform.position;
+        //med2DesPos = med2Des.transform.position;
+    }
+
+    public void Dragmed1()
+    {
+        med1.transform.position = Input.mousePosition;
+    }
+    public void Dragmed2()
+    {
+        med2.transform.position = Input.mousePosition;
+    }
+
+    public void Dropmed1()
+    {
+        float dis = Vector3.Distance(med1.transform.position, med1Des.transform.position);
+        if (dis < 30)
+        {
+            med1.transform.position = med1Des.transform.position;
+            Debug.Log("Correct");
+            med1Correct = true;
+        }
+        else
+        {
+            med1.transform.position = med1InitPos;
+            Debug.Log("InCorrect");
+            med2Correct = true;
+        }
+
+    }
+    public void Dropmed2()
+    {
+        float dis = Vector3.Distance(med2.transform.position, med2Des.transform.position);
+        if (dis < 30)
+        {
+            med2.transform.position = med2Des.transform.position;
+            Debug.Log("Correct");
+        }
+        else
+        {
+            med2.transform.position = med2InitPos;
+            Debug.Log("InCorrect");
+        }
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (med2Correct && med1Correct)
+        {
+            Debug.Log("Success");
+        }
+    }
+}

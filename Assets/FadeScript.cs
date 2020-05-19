60a7f8deae162421d7e3b3b241a77cc93652b7d4
Yangyeeun <kap12@naver.com>
2020-05-19 21:50:23 +0900

    Fade In,Out 실행
    일단 버튼누르면 fadein out하게 실행됨.
    퍼즐을 풀고 완료 버튼을 누르게만들면
    barrel이 무너지며 연기가 나고 fade out되며 게임종료 예정

---
+++ Assets/FadeScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FadeScript : MonoBehaviour
+{
+    public Image Panel;
+    float time = 0f;
+    float f_time = 1f;
+    public void Fade()
+    {
+        StartCoroutine(FadeFlow());
+    }
+    
+    IEnumerator FadeFlow()
+    {
+        Panel.gameObject.SetActive(true);
+        Color alpha = Panel.color;
+        time = 0;
+        while (alpha.a < 1f)
+        {
+            time += Time.deltaTime / f_time;
+            alpha.a = Mathf.Lerp(0,4, time);//시간에따라 흐려짐
+            Panel.color = alpha;
+            yield return null;
+        }
+        time = 0;
+        yield return new WaitForSeconds(1f);
+        while (alpha.a >0f)
+        {
+            time += Time.deltaTime / f_time;
+            alpha.a = Mathf.Lerp(4, 0, time);//시간에따라 흐려짐
+            Panel.color = alpha;
+            yield return null;
+        }
+        Panel.gameObject.SetActive(false);
+        yield return null; 
+    }
+}
